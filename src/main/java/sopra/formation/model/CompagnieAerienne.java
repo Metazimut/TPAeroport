b95598cf6ae7c7bb2081ddae1c23adde0cbaabcf
Stan <stanislas@gmail.com>
2021-07-12 15:57:44 +0200

    PartStan

---
+++ src/main/java/sopra/formation/model/CompagnieAerienne.java
@@ -0,0 +1,76 @@
+package sopra.formation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Company")
+public class CompagnieAerienne {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name="number")
+	private String code;
+	@Column(name="name")
+	private String nom;
+	@OneToMany(mappedBy="compagnieAerienne")
+	private CompagnieAerienneVol compagnieAerienneVol;
+	
+	
+public CompagnieAerienne() {
+	super();
+}
+
+public CompagnieAerienne(String code, String nom) {
+	this.code=code;
+	this.nom=nom;
+}
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getCode() {
+	return code;
+}
+
+public void setCode(String code) {
+	this.code = code;
+}
+
+public String getNom() {
+	return nom;
+}
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
+public CompagnieAerienneVol getCompagnieAerienneVol() {
+	return compagnieAerienneVol;
+}
+
+public void setCompagnieAerienneVol(CompagnieAerienneVol compagnieAerienneVol) {
+	this.compagnieAerienneVol = compagnieAerienneVol;
+}
+
+@Override
+public String toString() {
+	return "CompagnieAerienne [id=" + id + ", code=" + code + ", nom=" + nom + "]";
+}
+
+
+
+}
---
+++ src/main/java/sopra/formation/repositoryJpa/AeroportRepositoryJpa.java
@@ -0,0 +1,129 @@
+package sopra.formation.repositoryJpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.formation.appli.Application;
+import sopra.formation.model.Aeroport;
+
+import sopra.formation.repository.IAeroportRepository;
+
+public class AeroportRepositoryJpa implements IAeroportRepository {
+
+	public List<Aeroport> findAll() {
+		List<Aeroport> aeroports = new ArrayList<Aeroport>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Aeroport> query = em.createQuery("select a from Aeroport a", Aeroport.class);
+
+			aeroports = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return aeroports;
+	}
+
+	public Aeroport findById(Long id) {
+		Aeroport aeroport = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			aeroport = em.find(Aeroport.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return aeroport;
+	}
+
+	public Aeroport save(Aeroport obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	public void delete(Aeroport obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+}
