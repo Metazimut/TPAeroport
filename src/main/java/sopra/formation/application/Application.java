a67e3cb1eba98670fd9639f08d93c35502de9667
Aurélien <aurelien.dispot@gmail.com>
2021-07-12 16:11:55 +0200

    DAO end

--- src/main/java/sopra/formation/application/Application.java
+++ src/main/java/sopra/formation/application/Application.java
@@ -1,8 +1,26 @@
 package sopra.formation.application;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import sopra.formation.repository.IBilletRepository;
+import sopra.formation.repository.IPassagerRepository;
+import sopra.formation.repository.IReservationRepository;
+import sopra.formation.repository.IVolRepository;
+import sopra.formation.repository.jpa.BilletRepositoryJpa;
+import sopra.formation.repository.jpa.PassagerRepositoryJpa;
+import sopra.formation.repository.jpa.ReservationRepositoryJpa;
+import sopra.formation.repository.jpa.VolRepositoryJpa;
+
 public class Application {
 	private static Application instance = null;
+	
+	private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("tpaeroport");
 
+	private final IBilletRepository billetRepo = new BilletRepositoryJpa();
+	private final IPassagerRepository passagerRepo = new PassagerRepositoryJpa();
+	private final IReservationRepository reservationRepo = new ReservationRepositoryJpa();
+	private final IVolRepository volRepo = new VolRepositoryJpa();
 
 	private Application() {
 	}
@@ -15,4 +33,23 @@ public class Application {
 		return instance;
 	}
 
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+	public IBilletRepository getBilletRepo() {
+		return billetRepo;
+	}
+
+	public IPassagerRepository getPassagerRepo() {
+		return passagerRepo;
+	}
+
+	public IReservationRepository getReservationRepo() {
+		return reservationRepo;
+	}
+
+	public IVolRepository getVolRepo() {
+		return volRepo;
+	}
 }
--- src/main/java/sopra/formation/repository/jpa/BilletRepositoryJpa.java
+++ src/main/java/sopra/formation/repository/jpa/BilletRepositoryJpa.java
@@ -1,7 +1,126 @@
 package sopra.formation.repository.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.formation.application.Application;
+import sopra.formation.model.Billet;
 import sopra.formation.repository.IBilletRepository;
 
 public class BilletRepositoryJpa implements IBilletRepository {
-	
+	public List<Billet> findAll() {
+		List<Billet> billets = new ArrayList<Billet>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Billet> query = em.createQuery("select f from Billet f", Billet.class);
+
+			billets = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return billets;
+	}
+
+	public Billet findById(Long id) {
+		Billet billet = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			billet = em.find(Billet.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return billet;
+	}
+
+	public Billet save(Billet obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	public void delete(Billet obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEntityManagerFactory().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
 }
